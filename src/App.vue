5f4af6086484c40766497e3b0b9e04cfb396bc44
nwachuku <nwachukwu16@hotmail.com>
2018-04-30 00:02:39 -0400

    first commit
    resume

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+      <app-header></app-header>
+      <app-welcome></app-welcome>
+      <app-about></app-about>
+      <app-services></app-services>
+      <app-recent-work></app-recent-work>
+      <app-contact></app-contact>
+      <app-footer></app-footer>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Welcome from "./components/Welcome.vue";
+import About from "./components/About.vue";
+import Services from "./components/Services.vue";
+import Contact from "./components/Contact.vue";
+import RecentWork from "./components/RecentWork.vue";
+import ServiceSingle from "./components/ServiceSingle.vue";
+import Footer from "./components/Footer.vue";
+
+export default {
+  components: {
+    "app-header": Header,
+    "app-welcome": Welcome,
+    "app-services": Services,
+    "app-contact": Contact,
+    "app-recent-work": RecentWork,
+    "app-about": About,
+    "app-footer": Footer,
+    "app-service-single": ServiceSingle
+  },
+  name: "app",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+
+</style>
